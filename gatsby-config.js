b3494b5551a7a873cf243581ad7699091f903c14
Lawrence Humphrey <lawrence.humphrey1@Lawrences-MacBook-Pro-2.local>
2019-05-23 12:57:58 -0500

    build changes, /design prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,11 @@
+const { PATH_PREFIX = '/' } = process.env;
+
 module.exports = {
+  pathPrefix: PATH_PREFIX,
   __experimentalThemes: [{ resolve: 'gatsby-theme-carbon', options: {} }],
+  siteMetadata: {
+    title: 'IBM AI Design',
+  },
   proxy: {
     prefix: "/api/0.1",
     url: "http://localhost:6003",
